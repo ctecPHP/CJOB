9afe93ff8a3c30fc310dd83b73c1e5938a826e4b
= <=>
2019-02-10 21:49:58 -0200

    Tool Upload

---
+++ CJOB001.bat
@@ -0,0 +1,7 @@
+@echo off
+Color 0a
+echo --------------------------EXECUTANDO JOB001 ON PROTHEUS 12 ---------------------------------
+"C:\TOTVS 12\ANS\Protheus\bin\smartclient\smartclient.exe" -q -p=U_CJOB001 -c=tcp -e=DEV -m -l
+
+
+
---
+++ CJOB_RUN.vbs
@@ -0,0 +1,3 @@
+Dim wshShell
+Set wshShell = CreateObject("WScript.Shell")
+wshShell.Run "CJOB001.bat", 0, false
